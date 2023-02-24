6bb60bbf339e56cd8c9cc0e229c4a26ce1686f62
DomDerrien <dominique.derrien@gmail.com>
2023-02-24 07:44:18 -0500

    feat/MOGA-1139 test purchase tracker service

--- Assets/TestControllerWithCallbacks.cs
+++ Assets/TestControllerWithCallbacks.cs
@@ -7,26 +7,4 @@ using UnityEngine;
 
 
 public class TestControllerWithCallbacks : MonoBehaviour {
-
-    private bool forceReload = true;
-
-    public void ToggleForceReload() {
-        forceReload = !forceReload;
-        Debug.Log(forceReload ? "Services will be now forced to reload" : "Services will be now always using the initial instance");
-    }
-
-    [ContextMenu("Test Configuration Service (CB)")]
-    public void TestConfigServiceCB() {
-    }
-
-    [ContextMenu("Test Authentication Service (CB)")]
-    public void TestAuthenticationServiceCB() {
-        UserService service = UserService.GetInstance();
-        service.Get(
-            "me",
-            null,
-            (User u) => { Debug.Log("User: " + u); },
-            (BaseError e) => { Debug.Log($"Error: {e.message}"); }
-        );
-    }
 }
